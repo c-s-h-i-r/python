63494ea4d97a11ea1cc2cab62b0977530f69f0d0
cbognar <chrisbognar28@gmail.com>
2015-05-24 12:24:02 -0400

    Initial commit of List-1 exercises

---
+++ List-1.py
@@ -0,0 +1,78 @@
+import unittest
+
+#my simple exception class
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+#List-1 methods codingbat
+class List1:
+
+        
+# Here's our unit tests.
+class StringTests(unittest.TestCase):
+    obj = String1()
+    
+#    def test1(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test2(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test3(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test4(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test5(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test6(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test7(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test8(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test9(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test10(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test11(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#    def test12(self):
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+#        self.assertEqual(self.obj.)
+def main():
+    unittest.main()
+    
+if __name__ == '__main__':
+    main()
+                    
+
+
+
+
+    
+
+
