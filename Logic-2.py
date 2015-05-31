1f7402ee57e1229af0155f2ea5f878b8b9b59396
chris <chrisbognar@hotmail.ca>
2015-05-31 17:03:19 -0400

    Finished find sum excluding duplicates method.

--- Logic-2.py
+++ Logic-2.py
@@ -10,10 +10,44 @@ class MyError(Exception):
 #List-1 methods codingbat
 class Logic2:
     
-# Here's our unit tests.
+    @staticmethod
+    def lone_sum(a, b, c):
+        """
+        a,b,c -- integers
+        Return their sum; 
+        If one of the values is a duplicate do not count it.
+        """
+        #set is unordered values - commonly used to remove duplicates 
+        #from list
+
+        orig = [a,b,c]
+        #create list of duplicates from input
+        dups = set([x for x in orig if orig.count(x) > 1])
+        #return sum of duplicate set - original set
+        return sum(list(set(orig)-dups))
+            
+            
+    @staticmethod
+    def lucky_sum(a,b,c):
+        """
+        a,b,c -- integers
+        Return their sum.
+        If a value is 13 then don't count it or the value to the right.
+        """
+        
+# Unit tests.
 class LogicTests(unittest.TestCase):
     obj = Logic2()
     
+    def testLoneSum(self):
+        self.assertEqual(self.obj.lone_sum(1,2,3), 6)
+        self.assertEqual(self.obj.lone_sum(3,2,3), 2)
+        self.assertEqual(self.obj.lone_sum(3,3,3), 0)
+    def test1(self):
+        self.assertEqual(self.obj.lucky_sum(1,2,3),6)
+        self.assertEqual(self.obj.lucky_sum(1,2,13),3)
+        self.assertEqual(self.obj.lucky_sum(1,13,3),1)
+        
     
     
 def main():
