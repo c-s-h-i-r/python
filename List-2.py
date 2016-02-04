0c0fa13ff55a714b53bb78eef4a322282eb7f3b7
chris <commits@outlook.com>
2016-02-04 12:15:17 -0500

    Finished List-2

--- List-2.py
+++ List-2.py
@@ -80,8 +80,38 @@ class List2:
             count += 1
         return sum(nums)
 
-
+    @staticmethod
+    def sum67(nums):
+        '''Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+        '''
+        # mark = False
+        # for ind, num in enumerate(nums):
+            # if num == 6:
+                # mark = True
+            # if mark:
+                # nums[ind] = 0
+            # if num == 7:
+                # mark = False
+        # return sum(nums)
+        exists6 = True
+        while exists6:
+            try:
+                indexOf6 = nums.index(6)
+                indexOf7 = nums.index(7,indexOf6)
+                del nums[indexOf6:indexOf7+1]
+            except:
+                exists6 = False
+        return sum(nums)
     
+    @staticmethod
+    def has22(nums):
+        '''
+            Given an array of ints, return True if the 
+            array contains a 2 next to a 2 somehwere.
+        '''
+        return (2,2) in zip(nums, nums[1:])
+        
+        
 # Here's our unit tests.
 class ListTests(unittest.TestCase):
     obj = List2()
@@ -92,7 +122,15 @@ class ListTests(unittest.TestCase):
         self.assertEqual(self.obj.count_hi('abc hi ho'), 1)
     def test_sum13(self):
         self.assertEqual(self.obj.sum13([1,2,2,1]), 6)
-        
+    def test_sum67(self):
+        self.assertEqual(self.obj.sum67([1, 2, 2]),5)
+        self.assertEqual(self.obj.sum67([1, 2, 2, 6, 99, 99, 7]), 5)
+        self.assertEqual(self.obj.sum67([1, 1, 6, 7, 2]),4)
+        self.assertEqual(self.obj.sum67([1, 6, 1, 7, 1, 6, 7, 2]), 4)
+    def test_has22(self):
+        self.assertEqual(self.obj.has22([1, 2, 2]) , True)
+        self.assertEqual(self.obj.has22([1, 2, 1, 2]) , False)
+        self.assertEqual(self.obj.has22([2, 1, 2]) , False)
 def main():
     unittest.main()
     
